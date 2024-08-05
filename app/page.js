f9ad245787035129a3c45f4793a80332ae8db728
Lerich Osay <lerichyaso@gmail.com>
2024-08-04 22:49:12 -0500

    add/remove item changes

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@
 import Image from "next/image";
 import { firestore } from "@/firebase";
 import { useState, useEffect } from 'react'
-import { Box, Button, Modal, Stack, TextField, ThemeProvider, Typography } from "@mui/material";
+import { Box, Button, Fab, Modal, Stack, TextField, ThemeProvider, Typography } from "@mui/material";
 import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 import theme from "./theme";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Home() {
 
@@ -31,14 +33,20 @@ export default function Home() {
 
     if (docSnap.exists()) {
       const { quantity } = docSnap.data()
-      if (quantity === 1) {
-        await deleteDoc(docRef)
-      } else {
-        await setDoc(docRef, { quantity: quantity - 1 })
-      }
+      await setDoc(docRef, { quantity: quantity - 1 })
     }
 
     await updateInventory()
+  }
+
+  const removeAllItems = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      await deleteDoc(docRef)
+    }
+
+    await updateInventory() 
   }
 
   const addItem = async (item) => {
@@ -66,102 +74,116 @@ export default function Home() {
 
   return (
     <ThemeProvider theme={theme}>
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      gap={2}
-    >
-      <Modal open={open} onClose={handleClosed}>
-        <Box
-          position="absolute"
-          top="50%"
-          left="50%"
-          width={400}
-          bgcolor="white"
-          border="2px solid #000"
-          boxShadow={24}
-          p={4}
-          display="flex"
-          flexDirection="column"
-          gap={3}
-          sx={{
-            transform: "translate(-50%, -50%)"
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap={2}
+      >
+        <Modal open={open} onClose={handleClosed}>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            width={400}
+            bgcolor="white"
+            border="2px solid #000"
+            boxShadow={24}
+            p={4}
+            display="flex"
+            flexDirection="column"
+            gap={3}
+            sx={{
+              transform: "translate(-50%, -50%)"
+            }}
+          >
+            <Typography variant="h6">Add Item</Typography>
+            <Stack width="100%" direction="row" spacing={2}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                value={itemName}
+                onChange={(e) => {
+                  setItemName(e.target.value)
+                }}
+              />
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  addItem(itemName)
+                  setItemName('')
+                  handleClosed()
+                }}
+              >
+                Add
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
+        <Button
+          variant="contained"
+          onClick={() => {
+            handleOpen()
           }}
         >
-          <Typography variant="h6">Add Item</Typography>
-          <Stack width="100%" direction="row" spacing={2}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              value={itemName}
-              onChange={(e) => {
-                setItemName(e.target.value)
-              }}
-            />
-            <Button
-              variant="outlined"
-              onClick={() => {
-                addItem(itemName)
-                setItemName('')
-                handleClosed()
-              }}
-            >
-              Add
-            </Button>
+          Add New Item
+        </Button>
+        <Box border="1px solid #333">
+          <Box
+            width="800px"
+            height="100px"
+            bgcolor="#ADD8E6"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="h2" color="#333">
+              Inventory Items
+            </Typography>
+          </Box>
+          <Stack width="800px" height="300px" spacing={2} overflow="auto">
+            {inventory.map(({ name, quantity }) => (
+              <Box
+                key={name}
+                width="100%"
+                minHeight="150px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bgcolor="#f0f0f0"
+                padding={5}
+              >
+                <Typography variant="h3" color="#333" textAlign="center">
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </Typography>
+                <Box
+                  width="40%"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  gap={4}
+                >
+                  <Button variant="text" minHeight="150px" onClick={() => addItem(name)} disabled={quantity > 9}>
+                    <AddIcon style={{ scale: 2 }}/>
+                  </Button>
+                  <Typography variant="h3" color="#333" textAlign="center">
+                    {quantity}
+                  </Typography>
+                  <Button variant="text" minHeight="150px" onClick={() => removeItem(name)} disabled={quantity < 2}>
+                    <RemoveIcon style={{ scale: 2 }}/>
+                  </Button>
+                </Box>
+                <Button variant="contained" onClick={() => removeAllItems(name)}>
+                  Remove
+                </Button>
+              </Box>
+            ))}
           </Stack>
         </Box>
-      </Modal>
-      <Button
-        variant="contained"
-        onClick={() => {
-          handleOpen()
-        }}
-      >
-        Add New Item
-      </Button>
-      <Box border="1px solid #333">
-        <Box
-          width="800px"
-          height="100px"
-          bgcolor="#ADD8E6"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Typography variant="h2" color="#333">
-            Inventory Items
-          </Typography>
-        </Box>
-        <Stack width="800px" height="300px" spacing={2} overflow="auto">
-          {inventory.map(({ name, quantity }) => (
-            <Box
-              key={name}
-              width="100%"
-              minHeight="150px"
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              bgcolor="#f0f0f0"
-              padding={5}
-            >
-              <Typography variant="h3" color="#333" textAlign="center">
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </Typography>
-              <Typography variant="h3" color="#333" textAlign="center">
-                {quantity}
-              </Typography>
-              <Button variant="contained" onClick={() => removeItem(name)}>
-                Remove
-              </Button>
-            </Box>
-          ))}
-        </Stack>
       </Box>
-    </Box>
     </ThemeProvider>
   );
 }
